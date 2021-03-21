581c789cfc3a6504a8d90e58cbe97a03e0568b4b
Pontyus <73607914+Pontyus@users.noreply.github.com>
2021-03-21 19:30:46 +0000

    Add files via upload

---
+++ Scripts/PY/ClassifierGraphs.py
@@ -0,0 +1,91 @@
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import matplotlib.pyplot as plt
+
+train = pd.read_csv('training_data.csv')
+test = pd.read_csv('testing_data.csv')
+validation = pd.read_csv('validation_data.csv')
+
+train_in = []
+test_in = []
+train_out = []
+test_out = []
+validation_in = []
+validation_out = []
+
+for row in train.itertuples():
+    # train_in.append(row.Word)
+    train_out.append(row.Number_of_Syllables)
+    if row.Word == '                ':  # an empty word was getting in for some reason
+        pass
+    else:
+        temp = list(row.Word)
+        for i in range(len(temp)):
+            temp[i] = ord(temp[i])
+        train_in.append(temp)
+
+for row in test.itertuples():
+    # test_in.append(row.Word)
+    test_out.append(row.Number_of_Syllables)
+    if row.Word == '                ':
+        pass
+    else:
+        temp = list(row.Word)
+        for i in range(len(temp)):
+            temp[i] = ord(temp[i])
+        test_in.append(temp)
+
+for row in validation.itertuples():
+    # test_in.append(row.Word)
+    validation_out.append(row.Number_of_Syllables)
+    if row.Word == '                ':
+        pass
+    else:
+        temp = list(row.Word)
+        for i in range(len(temp)):
+            temp[i] = ord(temp[i])
+        validation_in.append(temp)
+
+test_in = np.array(test_in)
+test_out = np.array(test_out)
+train_in = np.array(train_in)
+train_out = np.array(train_out)
+validation_in = np.array(validation_in)
+validation_out = np.array(validation_out)
+
+max_word = 143
+
+model = keras.Sequential()
+model.add(keras.layers.Embedding(max_word, 100))
+model.add(keras.layers.GlobalAveragePooling1D())
+model.add(keras.layers.Dense(100, activation=tf.nn.relu))
+model.add(keras.layers.Dense(50, activation=tf.nn.relu))
+model.add(keras.layers.Dense(8, activation=tf.nn.softmax))
+model.summary()
+
+model.compile(optimizer=tf.compat.v1.train.GradientDescentOptimizer(learning_rate=0.2, use_locking=False),
+              loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+history = model.fit(train_in, train_out, epochs=1000, batch_size=500, validation_data=(test_in, test_out), verbose=2)
+
+results = model.evaluate(validation_in, validation_out)
+print('Accuracy is', results[1])
+
+# summarize history for accuracy
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
